1e4a7afff7f8bf3d356b961e16aaf664cd5cd5ab
Naif Balbaid <balbaidnaif@gmail.com>
2023-08-08 20:14:43 +0300

    clean

--- models/Rider.py
+++
@@ -1,9 +0,0 @@
-from .Account import Account
-
-
-class Rider(Account):
-    pass
-
-
-    class PydanticMeta:
-        exclude = ("company", "company_id")
--- models/UserConnection.py
+++
@@ -1,9 +0,0 @@
-from websocket import WebSocket
-
-
-class UserConnection:
-
-    def __init__(self, id: str, type: str, websocket: WebSocket):
-        self.websocket = websocket
-        self.id = id
-        self.type = type
